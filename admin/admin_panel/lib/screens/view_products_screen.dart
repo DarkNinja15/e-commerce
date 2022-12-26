b948b657b8e60eb51e82de61e05dd1517deed0a5
Animesh Shukla <animeshshukla1518@gmail.com>
2022-12-27 01:42:47 +0530

    added basic search feature

--- admin/admin_panel/lib/screens/view_products_screen.dart
+++ admin/admin_panel/lib/screens/view_products_screen.dart
@@ -4,11 +4,44 @@ import 'package:admin_panel/widgets/product_tile.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
-class ViewProducts extends StatelessWidget {
+class ViewProducts extends StatefulWidget {
   const ViewProducts({super.key});
 
   @override
+  State<ViewProducts> createState() => _ViewProductsState();
+}
+
+class _ViewProductsState extends State<ViewProducts> {
+  List searchResult = [];
+  TextEditingController controller = TextEditingController();
+
+  void searchquery(String query) async {
+    final result = await FirebaseFirestore.instance
+        .collection('products')
+        .where(
+          'name',
+          // isLessThanOrEqualTo: query,
+          isLessThanOrEqualTo: query.toLowerCase(),
+        )
+        .get();
+
+    setState(() {
+      searchResult = result.docs.map((e) => e.data()).toList();
+    });
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    controller.dispose();
+    searchResult = [];
+  }
+
+  @override
   Widget build(BuildContext context) {
+    // print(controller.text.toString());
+    // print('......');
+    // print(searchResult);
     return Scaffold(
         drawer: const Drawerc(),
         appBar: AppBar(
@@ -23,6 +56,10 @@ class ViewProducts extends StatelessWidget {
                   horizontal: MediaQuery.of(context).size.width * 0.1,
                   vertical: MediaQuery.of(context).size.width * 0.1),
               child: TextField(
+                controller: controller,
+                onChanged: (query) {
+                  searchquery(query);
+                },
                 style: const TextStyle(),
                 keyboardType: TextInputType.text,
                 decoration: InputDecoration(
@@ -39,36 +76,63 @@ class ViewProducts extends StatelessWidget {
                 ),
               ),
             ),
-            Expanded(
-              child: StreamBuilder(
-                stream: FirebaseFirestore.instance
-                    .collection('products')
-                    .snapshots(),
-                builder: ((context,
-                    AsyncSnapshot<QuerySnapshot<Map<String, dynamic>>>
-                        snapshot) {
-                  if (snapshot.connectionState == ConnectionState.waiting) {
-                    return const Loading();
-                  }
-                  if (snapshot.data!.docs.isEmpty) {
-                    return const Center(
-                      child: Image(
-                        image: AssetImage(
-                          'assets/void.png',
-                        ),
-                      ),
-                    );
-                  }
-                  return ListView.builder(
-                      itemCount: snapshot.data!.docs.length,
-                      itemBuilder: ((context, index) {
-                        return ProductTile(
-                          snap: snapshot.data!.docs[index].data(),
+            controller.text == ''
+                ? Expanded(
+                    child: StreamBuilder(
+                      stream: FirebaseFirestore.instance
+                          .collection('products')
+                          .snapshots(),
+                      builder: ((context,
+                          AsyncSnapshot<QuerySnapshot<Map<String, dynamic>>>
+                              snapshot) {
+                        if (snapshot.connectionState ==
+                            ConnectionState.waiting) {
+                          return const Loading();
+                        }
+                        if (snapshot.data!.docs.isEmpty) {
+                          return const Center(
+                            child: Image(
+                              image: AssetImage(
+                                'assets/void.png',
+                              ),
+                            ),
+                          );
+                        }
+                        return ListView.builder(
+                          itemCount: snapshot.data!.docs.length,
+                          itemBuilder: ((context, index) {
+                            return ProductTile(
+                              snap: snapshot.data!.docs[index].data(),
+                            );
+                          }),
                         );
-                      }));
-                }),
-              ),
-            ),
+                      }),
+                    ),
+                  )
+                : searchResult.isEmpty
+                    ? Expanded(
+                        child: ListView(
+                          children: const [
+                            Image(image: AssetImage('assets/void.png')),
+                            Text(
+                              'Nothing to Show',
+                              textAlign: TextAlign.center,
+                              style: TextStyle(
+                                fontSize: 33,
+                              ),
+                            ),
+                          ],
+                        ),
+                      )
+                    : Expanded(
+                        child: ListView.builder(
+                            shrinkWrap: true,
+                            itemCount: searchResult.length,
+                            itemBuilder: (context, index) {
+                              // print(searchResult[index]['name']);
+                              return ProductTile(snap: searchResult[index]);
+                            }),
+                      )
           ],
         ));
   }

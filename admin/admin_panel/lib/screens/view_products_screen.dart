c914fb588549e5c4a7f623ac7c81c46187b42534
Animesh Shukla <animeshshukla1518@gmail.com>
2022-12-26 10:12:30 +0530

    front end of all products screen completed

---
+++ admin/admin_panel/lib/screens/view_products_screen.dart
@@ -0,0 +1,55 @@
+import 'package:admin_panel/shared/loading.dart';
+import 'package:admin_panel/widgets/drawer.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+
+class ViewProducts extends StatelessWidget {
+  const ViewProducts({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        drawer: const Drawerc(),
+        appBar: AppBar(
+          backgroundColor: const Color.fromRGBO(204, 82, 88, 1),
+          title: const Text('All Products'),
+          centerTitle: true,
+        ),
+        body: Column(
+          children: [
+            Expanded(
+              child: StreamBuilder(
+                stream: FirebaseFirestore.instance
+                    .collection('products')
+                    .snapshots(),
+                builder: ((context,
+                    AsyncSnapshot<QuerySnapshot<Map<String, dynamic>>>
+                        snapshot) {
+                  if (snapshot.connectionState == ConnectionState.waiting) {
+                    return const Loading();
+                  }
+                  if (snapshot.data!.docs.isEmpty) {
+                    return const Center(
+                      child: Image(
+                        image: AssetImage(
+                          'assets/void.png',
+                        ),
+                      ),
+                    );
+                  }
+                  return ListView.builder(
+                      itemCount: snapshot.data!.docs.length,
+                      itemBuilder: ((context, index) {
+                        return const ListTile(
+                          leading: Icon(Icons.abc),
+                          title: Text('data'),
+                        );
+                      }));
+                }),
+              ),
+            ),
+          ],
+        ));
+  }
+}
--- admin/admin_panel/lib/widgets/drawer.dart
+++ admin/admin_panel/lib/widgets/drawer.dart
@@ -1,5 +1,6 @@
 import 'package:admin_panel/screens/add_seller_screen.dart';
 import 'package:admin_panel/screens/home_page.dart';
+import 'package:admin_panel/screens/view_products_screen.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
@@ -32,7 +33,13 @@ class Drawerc extends StatelessWidget {
                 Icons.view_agenda,
               ),
               title: const Text('View all Products'),
-              onTap: () {},
+              onTap: () {
+                Navigator.of(context).pushReplacement(
+                  MaterialPageRoute(
+                    builder: (context) => const ViewProducts(),
+                  ),
+                );
+              },
             ),
             ListTile(
               leading: const Icon(
